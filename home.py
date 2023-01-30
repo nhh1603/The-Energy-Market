fe10fbca44267f77859400a164ca74c3c50da648
nhh1603 <nghiahieu12@gmail.com>
2023-01-30 19:44:54 +0100

    m

--- home.py
+++ home.py
@@ -1,9 +1,9 @@
 class Home:
     def __init__(self, id, production_rate, consumption_rate, trading_policy):
         self.id = int(id)
-        self.production_rate = round(production_rate, 3)
-        self.consumption_rate = round(consumption_rate, 3)
-        self.gap = round(self.production_rate - self.consumption_rate, 3)
+        self.production_rate = int(production_rate)
+        self.consumption_rate = int(consumption_rate)
+        self.gap = int(self.production_rate - self.consumption_rate)
         self.trading_policy = int(trading_policy)
         self.exchange_market = 0
         if self.gap < 0:
@@ -12,12 +12,9 @@ class Home:
             self.exchange_market = self.gap
 
     def update_rates(self, new_production_rate, new_consumption_rate):
-        self.production_rate = round(new_production_rate, 3)
-        self.consumption_rate = round(new_consumption_rate, 3)
-        self.gap = round(new_production_rate - new_consumption_rate, 3)
-
-    def set_exchange_market(self, exchange_market):
-        self.exchange_market = exchange_market
+        self.production_rate = int(new_production_rate)
+        self.consumption_rate = int(new_consumption_rate)
+        self.gap = int(new_production_rate - new_consumption_rate)
 
     def __str__(self):
         return (f"Home {self.id}: \nProduction Rate: {self.production_rate} \nConsumption Rate: {self.consumption_rate} \nGap: {self.gap} \nTrading Policy: {self.trading_policy} \n")
