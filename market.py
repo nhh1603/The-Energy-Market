c1e1bb76593f0c21ecd6d89658103e866a3b90c9
dttrinh0604 <72909644+dttrinh0604@users.noreply.github.com>
2023-01-30 19:32:45 +0100

    Add files via upload

--- market.py
+++ market.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process
+from multiprocessing import Process, Semaphore
 import random
 import time
 import sys
@@ -10,72 +10,69 @@ import socket
 
 HOST = "localhost"
 PORT = 1789
-
-energyPrice = 0.145
-nbSell = 0
-nbBuy = 0  	 
+storage = 1000000 #W
+energyTransaction = 0
+energyPrice = 0.145 #e/KWh
 
 #functions
-
-def sellEnergy(energy):
-	print("Selling %d" % energy)
-	energyInitial = energyInitial - energy
-	nbSell = nbSell + 1
-
-def buyEnergy(energy):
-	print("Buying %d" % energy)
-	energyInitial = energyInitial + energy
-	nbBuy = nbBuy + 1
 
 def priceEnergy():
 	coeffAtte = 0.99
 	#coeffWeather = weather()
-	coeffTrans = limTransaction()
-	energyPrice = coeffAtte * energyPrice + coeffTrans * 0.01
+	coeffTrans = coeffTransaction()
+	energyPrice = coeffAtte * energyPrice + coeffTrans * energyPrice
 	return energyPrice
-		
-def limTransaction():
-	coeff = random.random()
-	if nbBuy > 5:
-		print("Too much of buying, inscreasing price...")
-		return coeff
-	elif nbSell > 5:
-		print("Too much of selling, lowering price...")
-		return -coeff
-	else:
-		return 0
 
-#def weather(): # shared memory
+def handle_energy(energy):
+    storage = storage + energyTransaction
+    if energy < 0: #selling energy
+        print("Selling %d" % energy)
+        storage=storage-energy
+    elif energy > 0: #buying energy
+        print("Buying %d" % energy)
+        storage=storage+energy
+    elif energy == 0:
+        print("Nothing happened")
+
+def coeffTransaction():
+    if energyTransaction > 0:
+        print("Decreasing price!")
+        return -energyTransaction/1000000
+    elif energyTransaction < 0:
+        print("Inscreasing price!")
+        return energyTransaction/1000000
+    else:
+        print("Nothing happened!")
+        return 0
+
+#def weather(): shared memory
 
 if __name__ == "__main__":
- threads = []
-# connect with homes with sockets 
- print("Starting market.")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind((HOST, PORT))
-    server_socket.listen()
-    client_socket, addr = server_socket.accept()
-    with client_socket:
-        print(f"Connected by {addr}")
+    threads = []
+    semaphore = Semaphore(5)
+    #connect with homes with sockets 
+    print("Starting market.")
+    with semaphore:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((HOST, PORT))
+            server_socket.listen()
+            client_socket, addr = server_socket.accept()
+        with client_socket:
+                print(f"Connected by {addr}")
         while True:
             data = client_socket.recv(1024)
-            if data == 1:
-                p = threading.Thread(target=sellEnergy, args= (data))
-                p.start()
-                threads.append(p)
-                nbSell=+1
-            if data == 2:
-                q = threading.Thread(target=buyEnergy, args= (data))
-                q.start()
-                threads.append(q)
-                nbBuy=+1
-            if data == 3:
-                print("Terminating market.")
-                for thread in threads:
-                    thread.join()
-                client_socket.close()
+            handle_energy(data)
+            energyTransaction = energyTransaction + data
+            client_socket.close()
+            coeffTransaction()
+            energyPrice = priceEnergy()
+            energyTransaction=0
+            print("Energy price %d" % energyPrice)
+            value=input("Press y to send energy price to home(s) qnd continue this simulation\n")
+            if value == 'y':    
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((HOST, PORT))
+                    s.sendall(energyPrice)
+            else:
+                print("Thanks for using this simulation")
                 break
-    limTransaction()
-    energyPrice = priceEnergy()
-    print("Energy price %d" % energyPrice)
-    time.sleep(5)
